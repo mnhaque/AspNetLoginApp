df271318c6e4b05ea29fbaecd677a5517ad9bbac
Nasim <mdnasimulhaque@hotmail.com>
2018-04-26 03:17:58 +0530

    added api controller

---
+++ AuthenticationApplication/AuthenticationApplication/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthenticationApplication.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationApplication.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Users")]
+    public class UsersController : Controller
+    {
+        [HttpPost]
+        public bool Register(User user)
+        {
+            return false;
+        }
+        [HttpGet]
+        public bool Login(string userName, string pwd)
+        {
+            return false;
+        }
+    }
+}
---
+++ AuthenticationApplication/AuthenticationApplication/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace AuthenticationApplication.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool AcceptedTnC { get; set; }
+    }
+}
